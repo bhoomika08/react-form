e1b834ff2ef29a7916043d8ffd7988d6f8b99be1
Bhoomika <bhoomika@vinsol.com>
2020-03-27 21:53:20 +0530

    moved input field event handler to CustomInputField.js

--- src/App/Components/index.js
+++ src/App/Components/index.js
@@ -1,90 +1,24 @@
 import React from 'react';
+import * as data from 'data.js'
 import CustomInputField from 'App/SharedComponents/CustomInputField.js';
-
-const formFields = {
-  name: {
-    name: "name",
-    label: "Name",
-    type: "text",
-    isRequired: true,
-    placeholder: "Enter name",
-  },
-  email: {
-    name: "email",
-    label: "Email",
-    type: "text",
-    isRequired: true,
-    placeholder: "Enter email",
-  },
-  address: {
-    name: "address",
-    label: "Address",
-    type: "text",
-    isRequired: false,
-    placeholder: "Enter address",
-  },
-  contact: {
-    name: "contact",
-    label: "Contact",
-    type: "tel",
-    maxLength: '9',
-    isRequired: false,
-    placeholder: "Enter contact number",
-  }
-}
 
 class RegistrationForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      fields: {
-        name: '',
-        email: '',
-        address: '',
-        contact: ''
-      },
-      error: {},
-      isSubmit: false,
+      isSubmit: ''
     }
+    this.formRef = React.createRef();
     this.submitHandler = this.submitHandler.bind(this);
     this.displayDetailsOnSubmit = this.displayDetailsOnSubmit.bind(this);
     this.getFormFields = this.getFormFields.bind(this);
   }
 
-  handleChange = (event, field) => {
-    let error = this.validateError(field, event.target.value);
-    this.setState({
-      isSubmit: false,
-      fields: { ...this.state.fields, [field.name]: event.target.value },
-      error: { ...this.state.error, ...error }
-    });
-  }
-
-  handleBlur = (event, field) => {
-    let error = this.validateError(field, event.target.value);
-    this.setState({
-      error: { ...this.state.error, ...error }
-    });
-  };
-
-  validateError(field, val) {
-    if (field.isRequired && val.length === 0) {
-      return {
-        [field.name]: true
-      }
-    }
-    else {
-      return {
-        [field.name]: false
-      }
-    }
-  }
-
-  canBeSubmitted(event) {
-    event.preventDefault();
-    let isSubmit = Object.values(formFields).every(field => {
+  canBeSubmitted() {
+    let formElements = this.formRef.current.elements;
+    let isSubmit = Object.values(data.formFields).every(field => {
       if (field.isRequired) {
-        return this.state.fields[field.name].length > 0 ? true : false
+        return formElements.namedItem(field.name).value.length > 0 ? true : false
       }
       else {
         return true
@@ -94,48 +28,42 @@ class RegistrationForm extends React.Component {
   }
 
   submitHandler(event) {
-    let canSubmit = this.canBeSubmitted(event);
+    event.preventDefault();
+    let canSubmit = this.canBeSubmitted();
     if (canSubmit) {
       this.setState({
         isSubmit: true,
       })
     }
     else {
-      let error = {};
-      Object.values(formFields).forEach(field => {
-        let submitErrors = this.validateError(field, this.state.fields[field.name]);
-        error = { ...error, ...submitErrors }
-      })
       this.setState({
         isSubmit: false,
-        error: { ...this.state.error, ...error }
       })
     }
   }
 
   getFormFields() {
-    const { error, fields } = this.state;
     return (
-      Object.values(formFields).map(field => (
+      Object.values(data.formFields).map(field => (
         <div key={field.label} className="mt-10">
           <label htmlFor={field.name}>
             {field.isRequired ? `${field.label}*` : field.label}
           </label>
           <CustomInputField
-            field={field}
-            value={fields[field.name]}
-            onChange={(event) => this.handleChange(event, field)}
-            onBlur={(event) => this.handleBlur(event, field)}
-            error={error[field.name]} />
+            inputField={field}
+          />
         </div>
       ))
     )
   }
 
   displayDetailsOnSubmit() {
+    let formElements = this.formRef.current.elements
     return (
       <div>
-        {Object.entries(this.state.fields).map(([key, value]) => <p key={key}>{key}: {value}</p>)}
+        {Object.values(data.formFields).map(field =>
+          <p key={field.name}>{field.name}: {formElements.namedItem(field.name).value}</p>
+        )}
       </div>
     )
   }
@@ -144,7 +72,8 @@ class RegistrationForm extends React.Component {
     const { isSubmit } = this.state;
     return (
       <div className="container">
-        <form onSubmit={this.submitHandler}>
+        {isSubmit === false && <p className="red">Please fill required fields</p>}
+        <form ref={this.formRef} onSubmit={this.submitHandler}>
           {this.getFormFields()}
           <button type="submit">Submit</button>
         </form>
--- src/App/SharedComponents/CustomInputField.js
+++ src/App/SharedComponents/CustomInputField.js
@@ -1,23 +1,57 @@
 import React from 'react';
 
-const CustomInputField = (props) => {
-  const { field: { name, type, maxLength, placeholder, label }, error, onChange, onBlur, value} = props;
-  return (
-    <>
-      <input
-        id={name}
-        type={type}
-        maxLength={maxLength}
-        className={error ? "error-input" : ""}
-        onChange={onChange}
-        onBlur={onBlur}
-        value={value}
-        placeholder={placeholder}
-      >
-      </input>
-      {error && <p className="error-message">{label} is a required field.</p>}
-    </>
-  )
+class CustomInputField extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      error: ''
+    }
+  }
+
+  handleChange = (event, field) => {
+    let error = this.validateError(field, event.target.value);
+    this.setState({
+      value: event.target.value,
+      error: error
+    });
+  }
+
+  handleBlur = (event, field) => {
+    let error = this.validateError(field, event.target.value);
+    this.setState({
+      error: error
+    });
+  }
+
+  validateError(field, val) {
+    if (field.isRequired && val.length === 0) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  render() {
+    const { inputField: { name, label, type, maxLength, placeholder }, inputField } = this.props;
+    const { error, value } = this.state;
+    return (
+      <>
+        <input
+          id={name}
+          type={type}
+          value={maxLength ? value.slice(0, maxLength) : value}
+          placeholder={placeholder}
+          className={error ? "error-input" : ""}
+          onChange={(event) => this.handleChange(event, inputField)}
+          onBlur={(event) => this.handleBlur(event, inputField)}
+        >
+        </input>
+        {error && <p className="error-message red">{label} is a required field.</p>}
+      </>
+    )
+  }
 }
 
 export default CustomInputField;
